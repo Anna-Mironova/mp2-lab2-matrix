5abe755c903588c82473b6d1346be390621b40b3
Anna-Mironova <anyt.ka@bk.ru>
2015-11-01 21:20:39 +0300

    Добавила расчет времени

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -6,26 +6,33 @@
 // Тестирование верхнетреугольной матрицы
 
 #include <iostream>
+#include <time.h>
 #include "utmatrix.h"
 //---------------------------------------------------------------------------
 
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
+  TMatrix<int> a(50), b(50), c(50);
   int i, j;
+  clock_t start,finish;
+  double time;
 
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки представления треугольных матриц"
     << endl;
-  for (i = 0; i < 5; i++)
-    for (j = 0; j < 5-i; j++ )
+  for (i = 0; i < 50; i++)
+    for (j = 0; j < 50-i; j++ )
     {
       a[i][j] =  i * 10 + j;
       b[i][j] = (i * 10 + j) * 100;
     }
+  start=clock();
   c = a + b;
   cout << "Matrix a = " << endl << a << endl;
   cout << "Matrix b = " << endl << b << endl;
   cout << "Matrix c = a + b" << endl << c << endl;
+  finish=clock();
+  time=(double)(finish-start)/(double)CLOCKS_PER_SEC;
+  cout<<"time"<<time<<endl;
 }
 //---------------------------------------------------------------------------
